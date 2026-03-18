957a246a1e1c8a3e0cf3e0cd32d11d13df6cf6e4
woo0218 <jungwoo9093@gmail.com>
2026-03-18 14:34:05 +0900

    3강

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
+import { useState } from 'react'
 function App() {
-    return <>안녕</>
+    const [todos, setTodos] = useState(['공부하기', '코딩하기', '운동하기'])
+
+    const handleOnSubmit = (e) => {
+        e.preventDefault()
+        const form = e.target
+        setTodos([form.todo.value, ...todos])
+    }
+
+    return (
+        <>
+            <form onSubmit={handleOnSubmit}>
+                <input type="text" name="todo" />
+                <button type="submit">등록</button>
+            </form>
+            <ul>
+                {todos.map((todo, i) => (
+                    <li key={i}>{todo}</li>
+                ))}
+            </ul>
+        </>
+    )
 }
 
 export default App
